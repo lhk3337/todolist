845654d145c4e1f2e8e05b24504238bb3a3a94b4
lhk3337 <lhk3337@naver.com>
2021-11-30 15:02:36 +0900

    array key 설정

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -5,22 +5,20 @@ const TodoList = ({ todos, setTodos }) => {
     setTodos(todos.filter((task) => task.id !== id));
   };
   return (
-    <>
-      <ul>
-        {todos
-          .filter((itemfilter) => itemfilter !== "")
-          .map((item) => (
-            <div className="listContainer">
-              <li key={item.id}>{item.data}</li>
-              <span>
-                <button className="delBtn" onClick={() => delBtn(item.id)}>
-                  ✖︎
-                </button>
-              </span>
-            </div>
-          ))}
-      </ul>
-    </>
+    <ul>
+      {todos
+        .filter((itemfilter) => itemfilter !== "")
+        .map((item) => (
+          <div key={item.id} className="listContainer">
+            <li>{item.data}</li>
+            <span>
+              <button className="delBtn" onClick={() => delBtn(item.id)}>
+                ✖︎
+              </button>
+            </span>
+          </div>
+        ))}
+    </ul>
   );
 };
 
