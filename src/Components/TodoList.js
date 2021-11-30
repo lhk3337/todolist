842642c042b42a618813ccdda9d9ac92e53f3890
lhk3337 <lhk3337@naver.com>
2021-12-01 00:12:24 +0900

    complete todo #7

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -4,18 +4,33 @@ const TodoList = ({ todos, setTodos }) => {
   const delBtn = (id) => {
     setTodos(todos.filter((task) => task.id !== id));
   };
+  const completeBtn = (id) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === id) {
+          item.isComplete = !item.isComplete;
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <ul>
       {todos
         .filter((itemfilter) => itemfilter !== "")
         .map((item) => (
           <div key={item.id} className="listContainer">
-            <li>{item.data}</li>
-            <span>
-              <button className="delBtn" onClick={() => delBtn(item.id)}>
+            <li className={`${item.isComplete ? "complete" : ""}`}>{item.data}</li>
+            <div className="btnContainer">
+              <button className="btn" onClick={() => completeBtn(item.id)}>
+                ✔️
+              </button>
+              <button className="btn" onClick={() => delBtn(item.id)}>
                 ✖︎
               </button>
-            </span>
+              <button className="btn addBtn">➕</button>
+            </div>
           </div>
         ))}
     </ul>
