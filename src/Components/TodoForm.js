5e03d90b69a4c307c6df92148a88e5e6f6062d85
lhk3337 <lhk3337@naver.com>
2021-12-06 16:14:47 +0900

    isComplete 초기화

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -18,7 +18,7 @@ const Todo = ({ setTodos, todos }) => {
       return;
     }
     const id = Math.floor(Math.random() * 10000) + 1;
-    setTodos([...todos, { id, data: input }]);
+    setTodos([...todos, { id, data: input, isComplete: false }]);
     setInput("");
     inputRefFocus();
   };
