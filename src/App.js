5cbe6e8a9e8f1880d725314c538f325ac205fbb9
lhk3337 <lhk3337@naver.com>
2021-12-07 19:03:02 +0900

    localstorage getItem and setItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style/App.css";
 import TodoForm from "./Components/TodoForm";
 import TodoHeader from "./Components/TodoHeader";
 import TodoList from "./Components/TodoList";
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem("todos");
+    const initialValue = JSON.parse(saved);
+    return initialValue || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="app">
